cc04d97a5e7ba78dc0a5617c889f34807d4a6719
Russell <russelltaylor05@gmail.com>
2024-07-25 17:56:27 -0700

    init

--- index.js
+++ index.js
@@ -1,26 +1,60 @@
 const Hapi = require('@hapi/hapi');
 
 const init = async () => {
-    const server = Hapi.server({
-        port: 3000,
-        host: 'localhost'
-    });
+  const server = Hapi.server({
+    port: process.env.PORT || 3000,
+    host: process.env.HOST || 'localhost',
+    router: {
+      stripTrailingSlash: true
+    },      
+  });
 
-    server.route({
-        method: 'GET',
-        path: '/',
-        handler: (request, h) => {
-            return 'Hello, HapiJS!';
-        }
-    });
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request, h) => {
+      return `
+        <!DOCTYPE html>
+        <html>
+        <head>
+          <title>DrawScape</title>
+        </head>
+        <body>
+          <h1>DrawScape</h1>
+        </body>
+        </html>
+      `;
+    }
+  });
 
-    await server.start();
-    console.log('Server running on %s', server.info.uri);
+  server.route({
+    method: 'GET',
+    path: '/qrcode',
+    handler: (request, h) => {
+      return `
+        <!DOCTYPE html>
+        <html>
+        <head>
+          <title>Redirecting...</title>
+          <script type="text/javascript">
+            window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+          </script>
+        </head>
+        <body>
+          <p>If you are not redirected automatically, follow this <a href="https://example.com">link</a>.</p>
+        </body>
+        </html>
+      `;
+    }
+  });
+
+  await server.start();
+  console.log('Server running on %s', server.info.uri);
 };
 
 process.on('unhandledRejection', (err) => {
-    console.log(err);
-    process.exit(1);
+  console.log(err);
+  process.exit(1);
 });
 
 init();
